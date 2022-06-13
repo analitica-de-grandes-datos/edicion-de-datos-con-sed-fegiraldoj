d517856295abaa8b4be73a7246809dcba367f0e7
fegiraldoj <101747510+fegiraldoj@users.noreply.github.com>
2022-06-12 21:54:31 -0500

    test

--- tests.py
+++ tests.py
@@ -23,7 +23,7 @@ if os.system("bash question.sh data.csv > output.csv") != 0:
 with open("output.csv", "r", encoding="utf8") as f:
     lines = f.readlines()
 
-print(lines)    
+print(os.system("ls -l"))    
 if len(lines) != len(expected):
     raise Exception("Wrong number of lines")
 
